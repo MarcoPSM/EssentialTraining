6174f9546f0389550d54d70e5526126b2a89f3ce
Marco Martins <marco.martins@nokia.com>
2023-10-15 08:47:30 +0000

    FirstCommit

---
+++ working/working.cpp
@@ -0,0 +1,15 @@
+// pointers.cpp by Bill Weinman [bw.org]
+// updated 2022-05-21
+#include <iostream>
+
+using std::cout;
+
+int main() {
+  int x{7};
+  int y{42};
+  int *ip = &x;
+
+  printf("The value of x is %d\n", x);
+  printf("The value of y is %d\n", y);
+  printf("The value of *ip is %d\n", *ip);
+}
